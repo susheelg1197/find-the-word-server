78ebb03e24ecd3b4e8eb939bbd2ade4fe0cefc05
Susheel Gounder <susheel.g@peopleinteractive.in>
2021-06-07 23:07:21 +0530

    App.py file updated

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_session import Session
 import random
 import json
 import csv
-
+import os
 
 
 app = Flask(__name__)
@@ -135,4 +135,6 @@ def loadConfig():
     return {"status":"Loaded config successfully"}
 
 if __name__ == "__main__":
-  app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    app.run()
